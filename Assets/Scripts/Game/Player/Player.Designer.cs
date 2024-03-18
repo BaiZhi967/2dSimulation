702fa2d4e73dd7d8473d3b39ef85343da470ff86
WhiteZhi <moycx@qq.com>
2024-03-19 02:51:48 +0800

    玩家基础移动功能

---
+++ Assets/Scripts/Game/Player/Player.Designer.cs
@@ -0,0 +1,20 @@
+// Generate Id:b66af356-ae26-44ff-ae97-fcfc5cf78713
+using UnityEngine;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace WhiteZhi.SimulationGame
+{
+	public partial class Player
+	{
+
+		public UnityEngine.GameObject playerPrefab;
+
+		public UnityEngine.Animator playerAnimator;
+
+		public UnityEngine.Rigidbody2D rigidbody;
+
+		public UnityEngine.Transform playerTransform;
+
+	}
+}
---
+++ Assets/Scripts/Game/Player/Player.cs
@@ -0,0 +1,73 @@
+using System;
+using UnityEngine;
+using QFramework;
+
+// 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
+// 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
+namespace WhiteZhi.SimulationGame
+{
+	public partial class Player : ViewController
+	{
+		public PlayerInput playerInput;
+
+		public float walkSpeed;
+		public Vector2 inputDirection;
+		public Vector2 realDirection;
+
+		private void Awake()
+		{
+			playerInput = new PlayerInput();
+		}
+
+		private void Update()
+		{
+			//获取玩家移动输入
+			inputDirection = playerInput.Game.Move.ReadValue<Vector2>();
+		}
+
+		private void FixedUpdate()
+		{
+			Movement();//移动
+		}
+
+		private void OnEnable()
+		{
+			playerInput.Enable();
+		}
+
+		private void OnDisable()
+		{
+			playerInput.Disable();
+		}
+
+		
+		
+		/// <summary>
+		/// 玩家移动逻辑的处理
+		/// </summary>
+		private void Movement()
+		{
+			//如果没有输入 则直接返回
+			if (inputDirection.Equals(Vector2.zero))
+			{
+				return;
+			}
+			realDirection = inputDirection;
+			if (inputDirection.x != 0 && inputDirection.y != 0)
+			{
+				realDirection = new Vector2(inputDirection.x * 0.7f, inputDirection.y * 0.7f);
+			}
+
+			//玩家方向修改
+			if (realDirection.x > 0)
+			{
+				playerTransform.localScale = new Vector3(-1, 1, 1);
+			}
+			else if (realDirection.x < 0)
+			{
+				playerTransform.localScale = new Vector3(1, 1, 1);
+			}
+			rigidbody.MovePosition(rigidbody.position + realDirection * walkSpeed * Time.deltaTime);
+		}
+	}
+}
