066e5869e90242b008f46669ee4f48e9b7a1b98f
WhiteZhi <moycx@qq.com>
2024-03-27 04:09:58 +0800

    Debug专用脚本添加

---
+++ Assets/Scripts/ForDebug/ForDebugBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using QFramework;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using WhiteZhi.SimulationGame;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace ForDebug
+{
+    public class ForDebugBehaviour : MonoBehaviour
+    {
+        public bool focusOnStartGame;
+        private void Start()
+        {
+            DontDestroyOnLoad(this);
+#if UNITY_EDITOR
+            if (focusOnStartGame)
+            {
+                Selection.activeGameObject = this.gameObject;
+            }
+#endif
+        }
+
+        private void OnGUI()
+        {
+            IMGUIHelper.SetDesignResolution( 640,360);
+            GUILayout.Space( 10);
+            GUILayout.BeginHorizontal();
+            GUILayout.Space(10);
+            GUILayout.Label($"天数:{Global.Days.Value}");
+            GUILayout.EndHorizontal();
+        }
+
+        [Button("天数+1",ButtonSizes.Small)]
+        public void AddDays()
+        {
+            Global.Days.Value++;
+        }
+        [Button("天数-1",ButtonSizes.Small)]
+        public void SubDays()
+        {
+            Global.Days.Value--;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Game/Global.cs
@@ -0,0 +1,12 @@
+﻿using QFramework;
+
+namespace WhiteZhi.SimulationGame
+{
+    public class Global
+    {
+        /// <summary>
+        /// 游戏天
+        /// </summary>
+        public static BindableProperty<int> Days = new BindableProperty<int>(1);
+    }
+} 
